286c84672172fcbd63c741e703a044136e2f30f8
Karthick <karthi.vels@gmail.com>
2017-08-14 16:19:16 +0530

    initi commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+s
+Capp/src/main/java/com/thapovan/androidlibsexample/MainActivity.java,3/d/3d6b5ec3d328adc42de07ba97260669c977d7b81
---
+++ androidlib/src/main/java/com/thapovan/androidlibs/TextUtil.java
@@ -0,0 +1,55 @@
+package com.thapovan.androidlibs;
+
+import android.widget.EditText;
+
+public class TextUtil {
+
+	private TextUtil() {
+	}
+
+	public static String cleanupString(Object obj) {
+		if(obj == null) return "";
+		if(obj instanceof EditText){
+			return ((EditText) obj).getText().toString().trim();
+		}else{
+			return cleanupString((String)obj);
+		}
+	}
+
+	public static String cleanupString(String str){
+		if(!TextUtil.isValidString(str)) return "";
+		return str.trim();
+	}
+
+
+	public static boolean isValidString(EditText editText){
+		if(editText==null) return false;
+		return isValidString(editText.getText().toString());
+	}
+//	Method to check text empty or null
+	public static boolean isValidString(String string) {
+		if(string == null){
+			return false;
+		}else if(string.trim().length() <= 0){
+			return false;
+		}else if(string.equals("null")){
+			return false;
+		}else {
+			return true;
+		}
+	}
+
+	// Workaround for EditText Cursor position
+	public static void setText(EditText editText,String text){
+		editText.setText("");
+		if(TextUtil.isValidString(text)){
+			editText.append(text);
+		}
+	}
+
+	public static String capitalizeFirstLetter(String original) {
+		if (original == null || original.length() == 0) return original;
+		return original.substring(0, 1).toUpperCase() + original.substring(1).toLowerCase();
+	}
+
+}
