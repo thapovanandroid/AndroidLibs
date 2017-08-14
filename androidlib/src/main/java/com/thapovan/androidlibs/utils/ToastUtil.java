f9cf439a21c1360984ef3d7efd50c1ada3143e2c
Karthick <karthi.vels@gmail.com>
2017-08-14 16:46:00 +0530

    added ToastUtil

---
+++ androidlib/src/main/java/com/thapovan/androidlibs/utils/ToastUtil.java
@@ -0,0 +1,43 @@
+package com.thapovan.androidlibs.utils;
+
+import android.content.Context;
+import android.support.annotation.StringRes;
+import android.view.Gravity;
+import android.widget.Toast;
+import com.thapovan.androidlibs.TextUtil;
+
+public class ToastUtil {
+
+    public static void showToast(Context context, String message) {
+        showToast(context, message, Gravity.NO_GRAVITY);
+    }
+
+    public static void showToast(Context context, @StringRes int stringID){
+        if(context == null ) return;
+        showToast(context, context.getString(stringID));
+    }
+
+    public static void showToast(Context context, @StringRes int strId, int gravity){
+        if(context == null ) return;
+        showToast(context, context.getString(strId), gravity);
+    }
+
+    public static void showCenterToast(Context context, String message){
+        showToast(context, message, Gravity.CENTER);
+    }
+
+    public static void showCenterToast(Context context, @StringRes int stringID){
+        showCenterToast(context,context.getString(stringID));
+    }
+
+    public static void showToast(Context context, String message, int gravity) {
+        if (!TextUtil.isValidString(message)) return;
+        if(gravity == Gravity.NO_GRAVITY){
+            Toast.makeText(context,message, Toast.LENGTH_LONG).show();
+        }else{
+            Toast toast = Toast.makeText(context, message,Toast.LENGTH_LONG);
+            toast.setGravity(gravity,0,0);
+            toast.show();
+        }
+    }
+}
