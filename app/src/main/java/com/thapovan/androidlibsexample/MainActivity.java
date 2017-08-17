61100cfbe9d3cefebbe45d7d8cb1e00be887a287
Karthick <karthi.vels@gmail.com>
2017-08-17 12:13:44 +0530

    cleanUp

--- app/src/main/java/com/thapovan/androidlibsexample/MainActivity.java
+++ app/src/main/java/com/thapovan/androidlibsexample/MainActivity.java
@@ -3,6 +3,10 @@ package com.thapovan.androidlibsexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.thapovan.commonutils.dialog.DialogUtil;
+import com.thapovan.commonutils.text.TextUtil;
+import com.thapovan.commonutils.toast.ToastUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
